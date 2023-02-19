98dcab76f134783b533bd51d03d9664af1bad1cb
Cristian FLORUTA <florutacristian@gmail.com>
2023-02-19 22:41:45 +0200

    steps definitions for the feature

---
+++ steps/AdvancedSearchSteps.py
@@ -0,0 +1,26 @@
+from behave import *
+
+
+@When('AdvancedSearchPage: I enter "{product_name}" in Enter keyword or item number block')
+def step_impl(context, product_name):
+    context.advanced_search_object.enter_keyword_or_item_number(product_name=product_name)
+
+
+@When('AdvancedSearchPage: I select "<keyword_choice>" from keyword option list')
+def step_impl(context, keyword_choice):
+    context.advanced_search_object.select_keyword_option(keyword_choice=keyword_choice)
+
+
+@When('AdvancedSearchPage: I enter "{exclude_brand}" in Exclude words from your search')
+def step_impl(context, exclude_brand):
+    context.advanced_search_object.exclude_keyword(exclude_brand=exclude_brand)
+
+
+@When('I select Clothing, Shoes & Accessories')
+def step_impl(context):
+    context.advanced_search_object.select_category()
+
+
+@When('I click Search button')
+def step_impl(context):
+    context.advanced_search_object.click_search_button()
---
+++ steps/HomepageSearchSteps.py
@@ -0,0 +1,32 @@
+from behave import *
+
+
+@given('Homepage: I am on ebay.com homepage')
+def step_impl(context):
+    context.homepage_object.navigate_to_homepage()
+    context.homepage_object.accept_cookies()
+
+
+@when('Homepage: I enter "{product_name}" on search bar')
+def step_impl(context, product_name):
+    context.homepage_object.insert_search_value(product_name=product_name)
+
+
+@when('Homepage: I select "category_name" from all categories dropdown menu')
+def step_impl(context, category_name):
+    context.homepage_object.select_category(category_name=category_name)
+
+
+@when('Homepage: I click Search button')
+def step_impl(context):
+    context.homepage_object.click_search_button()
+
+
+@when('Homepage: I click on Advanced search text link')
+def step_impl(context):
+    context.homepage_object.click_advanced_search_link()
+
+
+@then('ResultPage: Minimum 1,000 results are retrieved')
+def step_impl(context):
+    pass
