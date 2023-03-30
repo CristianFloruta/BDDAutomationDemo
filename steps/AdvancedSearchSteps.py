48bbe5c98319b71b22b5643b8c8d91495c6dd971
Cristian FLORUTA <florutacristian@gmail.com>
2023-03-30 22:41:00 +0300

    T3-T10 steps implemented

--- steps/AdvancedSearchSteps.py
+++ steps/AdvancedSearchSteps.py
@@ -20,5 +20,59 @@ def step_impl(context, exclude_brand):
 def step_impl(context):
     context.advanced_search_object.click_search_button()
 
+@when('AdvancedSearchPage: I enter iphone in Enter keyword or item number block')
+def step_impl(context):
+    context.advanced_search_object.enter_iphone()
+
+
+@when('AdvancedSearchPage: I select Sold items from Search including box')
+def step_impl(context):
+    context.advanced_search_object.select_sold_item()
+
+
+@when('AdvancedSearchPage: I click Search button from page bottom')
+def step_impl(context):
+    context.advanced_search_object.click_search_button_bottom()
+
+
+@when('AdvancedSearchPage: I select buy it now from Buying Format block')
+def step_impl(context):
+    context.advanced_search_object.select_buy_it_now()
+
+
+@when('AdvancedSearchPage: I select New from Condition block')
+def step_impl(context):
+    context.advanced_search_object.select_new_as_condition()
+
+
+@when('AdvancedSearchPage: I select Free returns from Show Results block')
+def step_impl(context):
+    context.advanced_search_object.select_free_returns()
+
+
+@when('AdvancedSearchPage: I select Worldwide from Item Location block')
+def step_impl(context):
+    context.advanced_search_object.select_worldwide_as_item_location()
+
+
+@when('AdvancedSearchPage: I select Free Shipping from Shipping Options block')
+def step_impl(context):
+    context.advanced_search_object.select_free_shipping()
+
+@when('AdvancedSearchPage: I select NotSpecified from Condition block')
+def step_impl(context):
+    context.advanced_search_object.select_unspecified_as_conditions()
+
+
+@when('AdvancedSearchPage: I select Available To from Item Location block')
+def step_impl(context):
+    context.advanced_search_object.select_available_to()
+
+
+@when('AdvancedSearchPage: I select United Kingdom')
+def step_impl(context):
+    context.advanced_search_object.select_united_kingdom()
+
+
 
 
