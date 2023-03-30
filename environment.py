4417728b4a576b61ab2ec0430a09d96d863949ef
Cristian FLORUTA <florutacristian@gmail.com>
2023-03-30 22:37:06 +0300

    ResultPage object added to initialisation

--- environment.py
+++ environment.py
@@ -1,12 +1,14 @@
 from browser import Browser
 from pages.ebay_homepage import Homepage
 from pages.ebay_advaced_search_page import Advanced_Search
+from pages.ebay_result_page import ResultPage
 
 
 def before_all(context):
     context.browser = Browser()
     context.homepage_object = Homepage()
     context.advanced_search_object = Advanced_Search()
+    context.result_page_object = ResultPage()
 
 
 def after_all(context):
